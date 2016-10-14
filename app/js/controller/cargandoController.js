02ab1af99085cca72ba577bcfb75b073a79d4ba1
Benjamín Garrido Barreiro <benjamin.garrido@gmail.com>
2016-10-14 22:32:56 +0200

    bgarrido - añadimos ngMockE2E para simular la respuesta del servicio con un MOCK

--- app/js/controller/cargandoController.js
+++ app/js/controller/cargandoController.js
@@ -1,16 +1,17 @@
-/*global angular*/
-angular.module("GeneradorInformes").controller("CargandoController", ["$rootScope", "$scope", "$location", "InformeService", function ($rootScope, $scope, $location, InformeService) {
-    "use strict";
+/*global angular, progresoCarga*/
+angular.module('GeneradorInformes').controller('CargandoController', ['$rootScope', '$scope', '$location', 'InformeService', function ($rootScope, $scope, $location, InformeService) {
+    'use strict';
     $scope.iniciarCargaInforme = function () {
-        InformeService.generar($rootScope).then(function (resultado) {
-            $rootScope.resultadoInforme = resultado;
+        InformeService.generar($rootScope.informacionUsuario).then(function (resultado) {
+            $rootScope.resultadoInforme = resultado.data;
         }).catch(function (resultado) {
             $rootScope.resultadoInforme = {
                 error: true
                 , resultado: resultado
             };
+            $rootScope.resultadoInforme.resultado.nombre = $rootScope.informacionUsuario.nombre;
         }).finally(function (resultado) {
-            $location.path("/informe");
+            $location.path('/informe');
         });
     };
-}]);
+    }]);
--- app/js/mainController.js
+++ app/js/mainController.js
@@ -1,23 +1,30 @@
 /*global angular*/
-var angularApp = angular.module("GeneradorInformes", ["ui.bootstrap", "ngRoute"]);
-angularApp.controller("MainController", ["$scope", "$location", function ($scope, $location) {
+var dependencies = ['ui.bootstrap', 'ngRoute'];
+try {
+    angular.module("ngMockE2E");
+    dependencies.push("ngMockE2E");
+}
+catch (err) {}
+var angularApp = angular.module('GeneradorInformes', dependencies);
+angularApp.controller('MainController', ['$scope', '$location', 'InformeService', function ($scope, $location, InformeService) {
+    'use strict';
     $scope.initApp = function () {
-        $location.path("/registro");
+        $location.path('/registro');
     };
 }]);
-angularApp.config(["$locationProvider", "$routeProvider", function ($locationProvider, $routeProvider) {
-    "use strict";
+angularApp.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    'use strict';
     $locationProvider.html5Mode();
-    $routeProvider.when("/registro", {
-        templateUrl: "view/registroInformacion.tpl.html"
-        , controller: "RegistroController"
+    $routeProvider.when('/registro', {
+        templateUrl: 'view/registroInformacion.tpl.html'
+        , controller: 'RegistroController'
     });
-    $routeProvider.when("/cargando", {
-        templateUrl: "view/cargandoInforme.tpl.html"
-        , controller: "CargandoController"
+    $routeProvider.when('/cargando', {
+        templateUrl: 'view/cargandoInforme.tpl.html'
+        , controller: 'CargandoController'
     });
-    $routeProvider.when("/informe", {
-        templateUrl: "view/informeResultados.tpl.html"
-        , controller: "InformeController"
+    $routeProvider.when('/informe', {
+        templateUrl: 'view/informeResultados.tpl.html'
+        , controller: 'InformeController'
     });
-}])
+}]);
